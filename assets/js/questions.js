5f73594f235b54d2d3b8e47fff5dcc2f5a74dc44
Summer Jones <summerbjones6@gmail.com>
2021-03-14 23:40:11 -0500

    final touches

---
+++ assets/js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Commonly used data types do NOT include:",
+    answer: "alerts",
+    options: [
+      "strings",
+      "booleans",
+      "alerts",
+      "numbers"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The condition in an if/else statement is enclosed with ____________",
+    answer: "parenthesis",
+    options: [
+      "quotes",
+      "curly brackets",
+      "parenthesis",
+      "square brackets"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Arrays in javascript can be used to store __________",
+    answer: "all of the above",
+    options: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "String values must be enclosed with __________ when being assigned to variables",
+    answer: "parenthesis",
+    options: [
+      "commas",
+      "curly brackets",
+      "quotes",
+      "parenthesis"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "console.log",
+    options: [
+      "JavaScript",
+      "terminal/bash",
+      "for loops",
+      "console.log"
+    ]
+  },
+ 
+];
